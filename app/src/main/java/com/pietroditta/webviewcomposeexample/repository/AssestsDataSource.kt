98cb7ba54da852ea1a5c67219f84007023c7f6eb
Pietro Ditta <pietro.ditta@jakala.com>
2025-06-25 09:27:03 +0200

    refactored

--- app/src/main/java/com/pietroditta/webviewcomposeexample/repository/AssestsDataSource.kt
+++ app/src/main/java/com/pietroditta/webviewcomposeexample/repository/AssestsDataSource.kt
@@ -6,7 +6,7 @@ import java.nio.charset.Charset
 
 object AssetsDataSource {
 
-    private val TAG: String = AssetsDataSource.javaClass.name
+    private val TAG = AssetsDataSource::class.java.simpleName
 
     private const val ANDROID_BRIDGE_PLACEHOLDER = "ANDROID_BRIDGE_PLACEHOLDER"
     private const val CODE_PLACEHOLDER = "CODE_PLACEHOLDER"
@@ -16,26 +16,18 @@ object AssetsDataSource {
     fun loadWebAndroidBridge(context: Context): String {
         Log.d(TAG, "Load from assets webAndroidBridge.js")
 
-        val webAndroidBridge = context.loadFromAsset("webviewAndroidBridge.js")
+        val webAndroidBridge = context.readAsset("webviewAndroidBridge.js")
             .replace(ANDROID_BRIDGE_PLACEHOLDER, JavascriptInterfaceRepository.NATIVE_IDENTIFIER)
             .replace("\n", "")
 
-        val jsToInject = context.loadFromAsset("webViewJsToInject.js")
+        return context.readAsset("webViewJsToInject.js")
             .replace(CODE_PLACEHOLDER, webAndroidBridge)
-            .replace(EVENT_NAME_PLACEHOLDER, BRIDGE_AVAILABLE_EVENT_NAME).trimIndent()
-
-        return jsToInject
-
+            .replace(EVENT_NAME_PLACEHOLDER, BRIDGE_AVAILABLE_EVENT_NAME)
+            .trimIndent()
     }
 
-    private fun Context.loadFromAsset(filename: String): String {
-        val inputStream = assets.open(filename)
-        val size = inputStream.available()
-        val buffer = ByteArray(size)
-        inputStream.read(buffer)
-        inputStream.close()
-        return String(buffer, Charset.defaultCharset())
-    }
-
-
+    private fun Context.readAsset(filename: String): String =
+        assets.open(filename).use { inputStream ->
+            inputStream.readBytes().toString(Charset.defaultCharset())
+        }
 }
